d207831e374555d1f7e0f927e251364eeae5a055
bishar08 <abubushayra@gmail.com>
2020-11-15 12:56:44 -0600

    Today I start working on my 1st Big Project

---
+++ main.js
@@ -0,0 +1,7 @@
+// Function to generate password with user input
+
+
+  // Push new random special character to guaranteedCharacters
+
+
+  // Transform the result into a string and pass into writePassword
